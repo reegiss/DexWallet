80f931833d935df8a043a50265ad8e98582e809c
Regis Araujo Melo <regis@r3tecnologia.net>
2021-07-09 19:45:53 -0300

    commit struct initial

---
+++ DexWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DexWallet
+//
+//  Created by Regis Araujo Melo on 09/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
